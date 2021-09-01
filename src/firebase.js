4fdbc65aacbf027af9b5228c32329a4a9aa5d4e3
Perry Defayette <defayetteperry@gmail.com>
2021-09-01 17:55:16 -0400

    Tried stuff.

--- src/firebase.js
+++ src/firebase.js
@@ -13,6 +13,14 @@ const firebaseConfig = {
   messagingSenderId: "76829730434",
   appId: "1:76829730434:web:d6f6e6793d9b1fec03b69b",
   measurementId: "G-H8GX9GKZQB",
+
+  // apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
