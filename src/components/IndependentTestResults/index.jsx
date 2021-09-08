f030189de562137bbe4d1253b9298fa495b15a3a
Perry Defayette <defayetteperry@gmail.com>
2021-09-08 11:48:46 -0400

    Minor change.

--- src/components/IndependentTestResults/index.jsx
+++ src/components/IndependentTestResults/index.jsx
@@ -106,6 +106,7 @@ export default function IndependentResults() {
       <button
         onClick={() => {
           console.log(testRefs.current.length);
+          console.log(testRefs.current.length);
 
           testRefs.current.forEach((item, i) => {
             item();
